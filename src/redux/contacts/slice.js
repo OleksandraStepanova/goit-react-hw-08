5ea4e2537f2de7e0ab5e62549f3d2a30295d547d
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-06-13 22:57:33 +0200

    Update slice.js

--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchContacts, addContact, deleteContact, editContact } from "./operations";
+import { fetchContacts, addContact, deleteContact } from "./operations";
 
 const handlePending = state => {
     state.error = false;
