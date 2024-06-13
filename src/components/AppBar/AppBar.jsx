92c4a3dcc35fe4889ae8058e5c8100ba317475ae
OleksandraStepanova <oleksandra.stepanova@gmail.com>
2024-06-13 23:03:00 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import { AppBar, Box, Toolbar } from "@mui/material";
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
-import UserMenu from "../UserMenu/Usermenu";
+import UserMenu from "../UserMenu/UserMenu";
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selector";
 
